42bf94eeb5869b5c310b8cfa35584cacffc08e28
brettcox <brettcoxemail@yahoo.com>
2013-06-24 11:57:19 -0500

    Updating, sign-in, and sign-out :Commit 1

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,5 @@
 module SessionsHelper
+  
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
@@ -16,8 +17,13 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
   end
+  
 end
